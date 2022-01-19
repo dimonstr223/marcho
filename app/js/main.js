09cb2ddba9d332efbc1d086dcdd165afe35286e1
dimonstr223 <dimstukmikolu@gmail.com>
2022-01-19 05:58:10 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,11 @@ $(function(){
     // autoplaySpeed: 2000,
   })
   $('.wrapper').slick({})
+
+  $('.star').rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
 }); 
